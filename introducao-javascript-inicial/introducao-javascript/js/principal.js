a2a047a2196c29b6515f3155563fa399bb9d8629
Manoel / Wallison <programador2@pc4.com.br>
2017-05-05 09:14:40 -0300

    01 - Replicando a validação e o cálculo do IMC para todos os pacientes

--- introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -1,18 +1,41 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length ; i++) {
+	//ATRIBUINDO VALORES
+	var paciente = pacientes[i] ;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100 / 4 =>>> 25
-tdImc.textContent = imc;
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
 
+	var tdImc = paciente.querySelector(".info-imc");
+	//var imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100 / 4 =>>> 25
+	//tdImc.textContent = imc;
 
+	var alturaEhValida = true;
+	var pesoEhValido = true;
 
-console.log(imc);
+	//VALIDANDO O PESO
+	if(peso <= 0 || peso > 1000){
+	    console.log("Peso inválido");
+	    tdImc.textContent = "Peso inválido!";
+	    pesoEhValido = false;
+	}
+
+	///VALIDANDO O ALTURA
+	if(altura <= 0 || altura >= 3.00){
+	    console.log("Altura inválida");
+	    tdImc.textContent = "Altura inválida!";
+	    alturaEhValida = false;
+	}
+
+	if(pesoEhValido && alturaEhValida){
+	    var imc = peso / ( altura * altura);
+	    tdImc.textContent = imc.toFixed(2);
+	}
+}
