51cd3e9be3fed9fcdcfa665ede48faaa1211ce81
Francisco Wallison <franciscowallison@gmail.com>
2022-07-11 08:22:04 -0300

    init jogos

---
+++ jogos-inicial/Chris Courses - Pokemon/Chris Courses - Pokemon/Tiled/poke.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tuxmon-sample" tilewidth="12" tileheight="12" tilecount="1280" columns="32">
+ <image source="../../../../phaser-3-tilemap-blog-posts-master/phaser-3-tilemap-blog-posts-master/examples/post-1/assets/tilesets/tuxmon-sample.png" width="384" height="480"/>
+</tileset>
